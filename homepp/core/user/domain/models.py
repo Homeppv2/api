1b39f832c5228d6e636c022315f2b53449500f62
mors-e <mors@nemors.ru>
2023-04-24 15:07:50 +0300

    create user model

--- homepp/core/user/domain/models.py
+++ homepp/core/user/domain/models.py
@@ -1,12 +1,24 @@
-from typing import Optional
-from dataclasses import dataclass, field
+# from typing import Optional
+# from dataclasses import dataclass, field
+#
+# from homepp.core.common.types.user import UserId, Username, Email
+#
+#
+# @dataclass
+# class User:
+#     id: Optional[UserId] = field(init=False, default=None)
+#     username: Username
+#     email: Email
+#     hashed_password: str
 
-from homepp.core.common.types.user import UserId, Username, Email
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
 
+Base = declarative_base()
 
-@dataclass
-class User:
-    id: Optional[UserId] = field(init=False, default=None)
-    username: Username
-    email: Email
-    hashed_password: str
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String(50), nullable=False)
+    last_name = Column(String(50), nullable=False)
