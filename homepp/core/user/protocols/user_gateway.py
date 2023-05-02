f63bb8d0570ddfd2bc4d5298002b9f49ce176c96
mors-e <mors@nemors.ru>
2023-05-02 23:48:24 +0300

    user gateway

--- homepp/core/user/protocols/user_gateway.py
+++ homepp/core/user/protocols/user_gateway.py
@@ -1,20 +1,35 @@
 from typing import Protocol, Optional
 
-from homepp.core.common.types.user import UserId, Email
+from homepp.core.common.types.user import UserId, Username, Email, RawPassword
 from homepp.core.user.domain.models import User
 
 
-class UserWriteGateway(Protocol):
-    def create_user(self, user: User) -> None:
-        raise NotImplementedError
+class UserProtocol(Protocol):
+    name: Username
+    email: Email
+    hashed_password: RawPassword
 
 
-class UserReadGateway(Protocol):
+class UserWriteGateway:
+    def __init__(self, db):
+        self.db = db
+
+    def create_user(self, user_gateway: UserProtocol) -> UserProtocol:
+        user = User(name=self.name, email=self.email, hashed_password=self.hashed_password)
+        self.db.add(user)
+        self.db.commit()
+        return user
+
+class UserReadGateway:
+    def __init__(self, db):
+        self.db = db
+
     async def get_user_by_id(self, user_id: UserId) -> Optional[User]:
-        raise NotImplementedError
+        return self.db.query(User).filter(User.id == user_id).first()
 
     async def get_user_by_email(self, email: Email) -> Optional[User]:
-        raise NotImplementedError
+        return self.db.query(User).filter(User.email == email).first()
 
+    # Разве у User должно быть поле hw_key ?
     async def get_by_hw_key(self, hw_key: str) -> Optional[User]:
         raise NotImplementedError
